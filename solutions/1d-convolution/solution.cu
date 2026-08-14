c5928436945e93a06d643cc51f3346c8907be941
hX <k.sudheeradh@gmail.com>
2026-08-14 16:01:42 +0530

    Solve 1D Convolution

---
+++ solutions/1d-convolution/solution.cu
@@ -0,0 +1,23 @@
+#include <cuda_runtime.h>
+
+__global__ void convolution_1d_kernel(const float* input, const float* kernel, float* output,
+                                      int input_size, int kernel_size) {
+                                        const int idx = blockDim.x * blockIdx.x + threadIdx.x;
+                                        if (idx >= input_size - kernel_size + 1) return;
+                                        output[idx] = 0;
+                                        for (int i = 0; i < kernel_size; i++){
+                                                output[idx] += input[idx + i] * kernel[i];
+                                        }
+                                      }
+
+// input, kernel, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, const float* kernel, float* output, int input_size,
+                      int kernel_size) {
+    int output_size = input_size - kernel_size + 1;
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (output_size + threadsPerBlock - 1) / threadsPerBlock;
+
+    convolution_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, kernel, output, input_size,
+                                                              kernel_size);
+    cudaDeviceSynchronize();
+}
