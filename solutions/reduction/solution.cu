c55711a9cd0509e5758f2190a93773ac51dd2505
hX <k.sudheeradh@gmail.com>
2026-08-23 19:21:06 +0530

    Solve Reduction

--- solutions/reduction/solution.cu
+++ solutions/reduction/solution.cu
@@ -1,56 +1,54 @@
 #include <cuda_runtime.h>
 
-__global__ void final_sum(float* partials, float* output, int N){
-
+__global__ void final_sum(const float* d_partials, float* output, int N) {
     const int tid = threadIdx.x;
-    const int block_dim = blockDim.x;
-
-    __shared__ float stemp[256];
-    stemp[tid] = 0;
-    int idx;
-
-    for (int i = 0; i < N; i += block_dim) {
-        idx = i + tid;
-        if (idx < N) stemp[tid] += partials[idx];
-    }
-    __syncthreads();
-
-    for (int stride = block_dim / 2; stride > 0; stride /= 2) {
-        if (tid < stride) {
-            stemp[tid] += stemp[tid + stride];
-        }
-        __syncthreads();
-    }
-
-    if (tid == 0) {
-        *output = stemp[tid];
-    }
-}
-
-__global__ void sum_partials(const float* input, float* partials, int N){
-    const int tid = threadIdx.x;
+    const int idx = blockDim.x * blockIdx.x + threadIdx.x;
     const int block_dim = blockDim.x;
     const int block_idx = blockIdx.x;
 
-    const int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    __shared__ float smem[256];
 
-    __shared__ float stemp[256];
-    if (idx < N){
-        stemp[tid] = input[idx];
-    } else {
-        stemp[tid] = 0;
+    smem[tid] = 0;
+
+    for (int i = 0; i < N; i += block_dim) {
+        if (tid + i < N) smem[tid] += d_partials[tid + i];
     }
     __syncthreads();
 
     for (int stride = block_dim / 2; stride > 0; stride /= 2) {
         if (tid < stride) {
-            stemp[tid] += stemp[tid + stride];
+            smem[tid] += smem[tid + stride];
         }
         __syncthreads();
     }
 
-    partials[block_idx] = stemp[0];
+    if (tid == 0) *output = smem[0];
 
+}
+
+__global__ void reduce(const float* input, float* d_partials, int N) {
+    const int tid = threadIdx.x;
+    const int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    const int block_dim = blockDim.x;
+    const int block_idx = blockIdx.x;
+
+    __shared__ float smem[256];
+
+    if (idx < N) {
+        smem[tid] = input[idx];
+    } else {
+        smem[tid] = 0;
+    }
+
+    __syncthreads();
+
+    for (int stride = block_dim / 2; stride > 0; stride /= 2) {
+        if (tid < stride) {
+            smem[tid] += smem[tid + stride];
+        }
+        __syncthreads();
+    }
+    d_partials[block_idx] = smem[0];
 }
 
 // input, output are device pointers
@@ -61,9 +59,6 @@ extern "C" void solve(const float* input, float* output, int N) {
     float* d_partials = nullptr;
     cudaMalloc(&d_partials, blocksPerGrid * sizeof(float));
 
-    sum_partials<<<blocksPerGrid, threadsPerBlock>>>(input, d_partials, N);
+    reduce<<<blocksPerGrid, threadsPerBlock>>>(input, d_partials, N);
     final_sum<<<1, threadsPerBlock>>>(d_partials, output, blocksPerGrid);
-
-    cudaFree(d_partials);
 }
-
